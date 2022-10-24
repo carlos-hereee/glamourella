8e35f53b4f6b2a72a81ba05d15feda51d72a8509
ch04937 <97hernandez.c@gmail.com>
2022-10-24 13:44:05 -0700

    create header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import Footer from "./pages/Footer";
+import Header from "./pages/Header";
 import Landing from "./pages/Landing";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route exact path="/" element={<Landing />} />
-        <Route exact path="/*" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/about-us">About Us</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/booking">Booking</Link>
+      <Link to="/gallery">Gallery</Link>
+      <Link to="/contact-us">Contact Us</Link>
+    </header>
+  );
 };
 
 export default Header;
