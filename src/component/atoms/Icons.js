a258131c8bac71e12b42e0ddcd594b2ddc66d924
ch04937 <97hernandez.c@gmail.com>
2022-10-25 11:16:11 -0700

    styles calendar component

--- src/component/atoms/Icons.js
+++ src/component/atoms/Icons.js
@@ -1,4 +1,14 @@
 import {
+  fa0,
+  fa1,
+  fa2,
+  fa3,
+  fa4,
+  fa5,
+  fa6,
+  fa7,
+  fa8,
+  fa9,
   faCalendarAlt,
   faHome,
   faImage,
@@ -17,6 +27,16 @@ const Icons = ({ name, size }) => {
     booking: faCalendarAlt,
     gallery: faImage,
     "contact us": faPhone,
+    1: fa1,
+    2: fa2,
+    3: fa3,
+    4: fa4,
+    5: fa5,
+    6: fa6,
+    7: fa7,
+    8: fa8,
+    9: fa9,
+    0: fa0,
   };
   return <FontAwesomeIcon icon={icons[name]} size={size} />;
 };
--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -2,23 +2,47 @@ import { useContext, useEffect, useState } from "react";
 import { Calendar } from "react-calendar";
 import EventCard from "../component/molecules/EventCard";
 import { CalendarContext } from "../utils/CalendarContext";
+import moment from "moment";
+import Icons from "../component/atoms/Icons";
 
 const Booking = () => {
   const [value, onChange] = useState(new Date());
   const { getCalendarDay, events, calendar } = useContext(CalendarContext);
 
-  useEffect(() => {
-    if (value) {
-      getCalendarDay(value);
+  // useEffect(() => {
+  //   if (value) {
+  //     getCalendarDay(value);
+  //   }
+  // }, [value]);
+  const tileContent = (date) => {
+    const tileDate = moment(date).format("ddd MMM DD YYYY");
+    const event = events.filter((e) => {
+      return moment(e.start.dateTime).format("ddd MMM DD YYYY") === tileDate;
+    });
+    if (event.length) {
+      return (
+        <p className="required">
+          <Icons name={event.length} /> Remaining!
+        </p>
+      );
     }
-  }, [value]);
-
-  console.log("calendar", calendar);
-  console.log("events", events);
+  };
   return (
-    <div>
+    <div className="card">
       <h1>Booking {}</h1>
-      <Calendar onChange={onChange} value={value} minDate={value} />
+      <Calendar
+        onChange={onChange}
+        value={value}
+        minDate={new Date()}
+        minDetail="month"
+        prev2Label={null}
+        next2Label={null}
+        navigationLabel={({ label }) => (
+          <h3>{label.charAt(0).toUpperCase() + label.slice(1)}</h3>
+        )}
+        showNeighboringMonth={false}
+        tileContent={({ date }) => tileContent(date)}
+      />
       <p>{calendar.description}</p>
       {/* <EventCard data={event} /> */}
     </div>
