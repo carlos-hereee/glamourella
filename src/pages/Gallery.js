655d8bfb9c330d6bece2c840e234fcdaa86dd4c7
ch04937 <97hernandez.c@gmail.com>
2022-11-15 19:13:04 -0800

    shows 1 image

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,8 +1,29 @@
+import { useContext } from "react";
+import { GalleryContext } from "../utils/context/GalleryContext";
+
 const Gallery = () => {
+  const { gallery } = useContext(GalleryContext);
+  console.log("gallery", gallery);
   return (
-    <div>
-      <h2>Gallery</h2>
-    </div>
+    <section className="container">
+      <div className="card">
+        <h2>Gallery</h2>
+        <div className="gallery-photos">
+          {gallery &&
+            gallery.map((g) => (
+              <div key={g.uid} className="gallery-photo">
+                <img
+                  src={g.src}
+                  alt={g.fileName}
+                  className="photo"
+                  crossOrigin="anonymous"
+                />
+                <span>Credit to: {g.name}</span>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
