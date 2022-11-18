acb6e3f4ce8581f8c94279e6ebce73b00e61d088
ch04937 <97hernandez.c@gmail.com>
2022-11-17 16:15:10 -0800

    filter gallery with buttons

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -24,7 +24,8 @@ const Gallery = () => {
         <nav className="navbar">
           <Buttons name="all" handleClick={handleClick} />
           <Buttons name="wig" handleClick={handleClick} />
-          <Buttons name="nail" handleClick={handleClick} />
+          <Buttons name="braids" handleClick={handleClick} />
+          {/* <Buttons name="nail" handleClick={handleClick} /> */}
         </nav>
         <div className="gallery-photos">
           {isFiltered
