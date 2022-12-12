7bf170a38487bd7140822233615dbf7ef5091e0a
ch04937 <97hernandez.c@gmail.com>
2022-12-12 11:40:37 -0800

    show admin schedule on screen

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -4,20 +4,22 @@ import shortid from "shortid";
 import CustomLink from "../component/atoms/CustomLink";
 import { UserContext } from "../utils/context/UserContext";
 import MenuLink from "../component/molecules/MenuLink";
+import { CalendarContext } from "../utils/context/CalendarContext";
 
 const AdminDashboard = () => {
   // TODO: ADMIN ADD THEIR WORK SCHEDULE
   // TODO:
   const { user, booked } = useContext(UserContext);
+  const { formatDate } = useContext(CalendarContext);
   const [menu, setMenu] = useState([
     {
-      name: "My Schedule",
+      name: "Schedule",
       uid: shortid.generate(),
     },
   ]);
   const [schedule, setSchedule] = useState([
     {
-      day: "monday",
+      day: formatDate(new Date()),
       uid: shortid.generate(),
       shift: [
         { uid: shortid.generate(), time: "9am - 10am", isOpen: true },
@@ -77,51 +79,55 @@ const AdminDashboard = () => {
       ],
     },
   ]);
+  const appointment = (
+    <>
+      <h3>Upcoming appointments</h3>
+      {booked.length > 0 &&
+        booked.map((b) => (
+          <div key={b.uid}>
+            <h2>{b.type}</h2>
+            <p>Start count</p>
+          </div>
+        ))}
+    </>
+  );
   const handleClick = (e) => {
     let item = e.target.innerText;
+    menu.findIndex((m) => m.uid);
   };
-
+  // TODO, FORMAT DATE WITH COMA, AND PERIOD.
   return (
-    <div className="card-container">
-      <h2>Admin dashboard</h2>
-      <div className="card-admin">
-        <nav className="secondary-navigation">
-          <ul className="navigation" data-state="open">
-            {menu.map((m) => (
-              <MenuLink data={m} key={m.uid} handleClick={handleClick} />
-            ))}
-          </ul>
-        </nav>
-        <div className="card-secondary">
-          <h3>My schedule</h3>
-          {schedule.length > 0 &&
-            schedule.map((s) => (
-              <div key={s.uid}>
-                <h4>{s.day}</h4>
-                <div className="list">
-                  {s.shift.length > 0 &&
-                    s.shift.map((sh) => (
-                      <button
-                        type="button"
-                        className="btn list-item"
-                        key={sh.uid}>
-                        {sh.time}
-                      </button>
-                    ))}
-                </div>
+    <section className="admin-container">
+      <nav className="secondary-navigation">
+        <ul className="navigation" data-state="open">
+          {menu.map((m) => (
+            <MenuLink data={m} key={m.uid} handleClick={handleClick} />
+          ))}
+        </ul>
+      </nav>
+      <div className="card-secondary">
+        <h3>My schedule</h3>
+        {schedule.length > 0 &&
+          schedule.map((s) => (
+            <div className="admin-content" key={s.uid}>
+              <p>
+                <strong>{s.day}</strong>
+              </p>
+              <div className="list">
+                {s.shift.length > 0 &&
+                  s.shift.map((sh) => (
+                    <button
+                      type="button"
+                      className="btn list-item"
+                      key={sh.uid}>
+                      {sh.time}
+                    </button>
+                  ))}
               </div>
-            ))}
-          <h3>Upcoming appointments</h3>
-          {booked.length > 0 &&
-            booked.map((b) => (
-              <div key={b.uid}>
-                <h2>{b.type}</h2>
-                <p>Start count</p>
-              </div>
-            ))}
-        </div>
+            </div>
+          ))}
       </div>
-    </div>
+    </section>
   );
 };
 
