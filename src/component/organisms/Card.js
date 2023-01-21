b28d906448249df0be81961f7f1e5be26c486f88
ch04937 <97hernandez.c@gmail.com>
2023-01-21 15:32:14 -0800

    add for sale buttons to card

--- src/component/organisms/Card.js
+++ src/component/organisms/Card.js
@@ -1,19 +1,13 @@
 import CardHeader from "../molecules/card/CardHeader";
 import CardBody from "../molecules/card/CardBody";
-import AddToCart from "../molecules/AddToCart";
-import RemoveFromCart from "../molecules/RemoveFromCart";
+import ForSaleBtn from "../molecules/buttons/ForSaleBtn";
 
-const Card = ({ data, cart }) => {
+const Card = ({ data }) => {
   return (
     <div className="card">
       <CardHeader data={data} />
       <CardBody data={data} />
-
-      {data.isForSale && cart.filter((c) => c.uid === data.uid).length > 0 ? (
-        <RemoveFromCart data={data} />
-      ) : (
-        <AddToCart data={data} />
-      )}
+      {data.isForSale && <ForSaleBtn data={data} />}
     </div>
   );
 };
