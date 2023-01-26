eaa1e959a57bb38021c35d29195b6a19910554d1
ch04937 <97hernandez.c@gmail.com>
2023-01-25 21:41:49 -0800

    update total when quantity changes

--- src/component/molecules/InStorePurchase.js
+++ src/component/molecules/InStorePurchase.js
@@ -1,41 +1,11 @@
-import { useContext } from "react";
-import { ServicesContext } from "../../utils/context/ServicesContext";
-import AccessoryDetails from "../atoms/AccessoryDetails";
-import Cost from "../atoms/Cost";
-import MeetingDetails from "../atoms/MeetingDetails";
 import CardHeader from "./card/CardHeader";
-import * as yup from "yup";
-import Field from "../organisms/Field";
 
-const schema = yup.object().shape({ quantity: yup.number() });
-const values = { quantity: 1 };
 const InStorePurchase = () => {
-  const { cart, onQuantityChange } = useContext(ServicesContext);
-
   return (
     <div className="secondary-card-section">
       <CardHeader
         data={{ title: "Payment type: In Store", subtitle: "Summary Details" }}
       />
-      <div className="card-section-wrapper">
-        {cart.map((c) => (
-          <div className="card-section-row" key={c.uid}>
-            {c.isAccessory ? (
-              <AccessoryDetails data={c} />
-            ) : (
-              <MeetingDetails meeting={c.meeting} />
-            )}
-            {c.isAccessory && (
-              <Field
-                data={{ values, schema }}
-                max={c.inStock}
-                change={({ target }) => onQuantityChange(target.value, c)}
-              />
-            )}
-            {c.cost && c.count && <Cost cost={c.cost * c.count} />}
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
--- src/component/organisms/PaymentMethods.js
+++ src/component/organisms/PaymentMethods.js
@@ -3,20 +3,28 @@ import { AppContext } from "../../utils/context/AppContext";
 import { ServicesContext } from "../../utils/context/ServicesContext";
 import Buttons from "../molecules/buttons/Buttons";
 import InStorePurchase from "../molecules/InStorePurchase";
+import * as yup from "yup";
+import AccessoryDetails from "../atoms/AccessoryDetails";
+import MeetingDetails from "../atoms/MeetingDetails";
+import Cost from "../atoms/Cost";
+import Field from "./Field";
+
+const schema = yup.object().shape({ quantity: yup.number() });
+const values = { quantity: 1 };
 
 const PaymentMethods = () => {
   const { paymentMethods, selectPaymentType, paymentType } = useContext(AppContext);
   const [total, setTotal] = useState(0);
-  const { cart } = useContext(ServicesContext);
+  const { cart, onQuantityChange } = useContext(ServicesContext);
 
-  // useEffect(() => {
-  //   if (cart.length > 0) {
-  //     let cost = cart.reduce((a, b) => {
-  //       return a + b.cost;
-  //     }, 0);
-  //     setTotal(cost);
-  //   }
-  // }, [cart]);
+  useEffect(() => {
+    if (cart.length > 0) {
+      let cost = cart.reduce((a, b) => {
+        return a + b.cost * b.count;
+      }, 0);
+      setTotal(cost);
+    }
+  }, [cart]);
   const handleClick = (e) => {
     selectPaymentType(e);
   };
@@ -33,6 +41,26 @@ const PaymentMethods = () => {
         ))}
       </nav>
       {paymentType.uid && payment[paymentType.type]}
+      <div className="card-section-wrapper">
+        {cart.map((c) => (
+          <div className="card-section-row" key={c.uid}>
+            {c.isAccessory ? (
+              <AccessoryDetails data={c} />
+            ) : (
+              <MeetingDetails meeting={c.meeting} />
+            )}
+            {c.isAccessory && (
+              <Field
+                data={{ values, schema }}
+                max={c.inStock}
+                change={({ target }) => onQuantityChange(target.value, c)}
+              />
+            )}
+            {c.cost && c.count && <Cost cost={c.cost * c.count} />}
+          </div>
+        ))}
+      </div>
+
       <h3 className="total">Total ${total}</h3>
     </div>
   );
